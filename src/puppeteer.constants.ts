f1b4a559880daf34baa8c57618d396514cc29b6b
Konstantin Vyatkin <tino@vtkn.io>
2020-06-17 12:58:02 -0400

    add Puppeteer types

--- src/puppeteer.constants.ts
+++ src/puppeteer.constants.ts
@@ -1,9 +1,9 @@
-import { MongoClientOptions } from 'mongodb';
+import { LaunchOptions } from 'puppeteer';
 
 export const MONGO_CONNECTION_NAME = 'MongoConnectionName';
 export const MONGO_MODULE_OPTIONS = 'MongoModuleOptions';
 
 export const DEFAULT_MONGO_CONNECTION_NAME = 'DefaultMongo';
-export const DEFAULT_MONGO_CLIENT_OPTIONS: MongoClientOptions = {
-  useNewUrlParser: true,
+export const DEFAULT_CHROME_LAUNCH_OPTIONS: LaunchOptions = {
+  headless: true,
 };
