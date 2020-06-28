b7e3479b33dd1e8e1126804bd7c902cad02513f6
Konstantin Vyatkin <tino@vtkn.io>
2020-06-28 12:20:13 -0400

    add --no-zygote

--- src/puppeteer.constants.ts
+++ src/puppeteer.constants.ts
@@ -8,15 +8,15 @@ export const DEFAULT_PUPPETEER_INSTANCE_NAME = 'DefaultPuppeteer';
 const args: LaunchOptions['args'] = [
   '--allow-insecure-localhost', // Enables TLS/SSL errors on localhost to be ignored (no interstitial, no blocking of requests).
   '--allow-http-screen-capture', // Allow non-secure origins to use the screen capture API and the desktopCapture extension API.
+  '--no-zygote', // https://codereview.chromium.org/2384163002
 ];
-// add --no-sandbox when running as root on Linux
-if (typeof process.getuid === 'function' && process.getuid() === 0) {
-  // console.debug('Running as root on Linux, so, add --no-sandbox to Puppeteer!');
+// add --no-sandbox when running on Linux, required with --no-zygote
+if (typeof process.getuid === 'function') {
   args.push('--no-sandbox');
 }
 
 export const DEFAULT_CHROME_LAUNCH_OPTIONS: LaunchOptions = {
   headless: true,
-  pipe: true,
+  pipe: process.platform !== 'win32',
   args,
 };
